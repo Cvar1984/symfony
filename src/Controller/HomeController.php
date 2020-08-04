bfc53dab480cec09dd072dfe47a9fa56c92f1aeb
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-08-05 01:54:30 +0800

    initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('home.html.twig', [
+            'from' => 'twig'
+        ]);
+    }
+}
